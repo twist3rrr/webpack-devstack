1091e90035436340e8fa6c18be33e57b085e7a63
Roman Pilip <roman.pilip@actum.cz>
2018-04-27 08:44:23 +0200

    add html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/js/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist', 'js')
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
@@ -15,5 +16,11 @@ module.exports = {
                 loader: "babel-loader" 
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src', 'tpl', 'index.html')
+        })
+    ]
 }
